093ba52e958e5d9928edbe3d41cc83cd10c9c531
barneyha <barneyha@dukes.jmu.edu>
2015-12-04 05:41:48 -0500

    Update challenge.js

--- css-challenge_files/challenge.js
+++ css-challenge_files/challenge.js
@@ -1,6 +1,6 @@
 $(document).ready(function() { 
     $('fa fa-twitter').mouseenter(function() { 
-        $('fa fa-twitter').fadeTo('fast', 1); 
+        $('fa fa-twitter').hide; 
     }); 
     $('fa fa-twitter').mouseleave(function() { 
         $('fa fa-twitter').fadeTo('fast',0.5); 
