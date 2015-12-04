91941edb85ab8f1e390045feac8d062a0d60bef5
barneyha <barneyha@dukes.jmu.edu>
2015-12-04 05:33:08 -0500

    Delete challenge.js

--- css-challenge_files/challenge.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('i').mouseenter(function() {
-        $('i').fadeTo('fast', 1);
-    });
-    $('i').mouseleave(function() {
-        $('i').fadeTo('fast',0.5);
-    });
-});
